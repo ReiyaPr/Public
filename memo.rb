a022ca977d143bba6ad01f3b01ad7a286423997d
ReiyaPr <oas38t21151067j@gmail.com>
2021-06-12 16:38:23 +0900

    modified connect and load method  & delete edit_id

--- memo.rb
+++ memo.rb
@@ -2,57 +2,54 @@
 
 require 'sinatra'
 require 'sinatra/reloader'
-require 'json'
 require 'pg'
 
 class Memo
   class << self
-    def create_db
-      db = PG.connect(dbname: 'postgres')
-      db.exec 'CREATE TABLE IF NOT EXISTS Memos(Id serial PRIMARY KEY, Title text NOT NULL, Article text NOT NULL, Edit_Id VARCHAR(36))'
+    def create_db(db)
+      db.exec 'CREATE TABLE IF NOT EXISTS Memos(Id serial PRIMARY KEY, Title text NOT NULL, Article text NOT NULL)'
     end
 
-    def load_memo(edit_id = nil)
-      db = PG.connect(dbname: 'postgres')
-      if edit_id
-        db.prepare('load', 'SELECT * FROM Memos WHERE edit_id = $1')
-        db.exec_prepared('load', [edit_id])
-      else
-        db.exec('SELECT * FROM Memos')
-      end
+    def load_all_memos(db)
+      db.exec('SELECT * FROM Memos')
     end
 
-    def add_memo(title, article, edit_id)
-      db = PG.connect(dbname: 'postgres')
-      db.prepare('add', 'INSERT INTO Memos (title, article, edit_id) VALUES ($1, $2, $3)')
-      db.exec_prepared('add', [title, article, edit_id])
+    def prepared_statesments(db)
+      db.prepare('load', 'SELECT * FROM Memos WHERE id = $1')
+      db.prepare('add', 'INSERT INTO Memos (title, article) VALUES ($1, $2)')
+      db.prepare('delete', 'DELETE FROM Memos WHERE id = $1')
+      db.prepare('update', 'UPDATE Memos SET title = $1, article = $2 WHERE id = $3')
     end
 
-    def delete_memo(edit_id)
-      db = PG.connect(dbname: 'postgres')
-      db.prepare('delete', 'DELETE FROM Memos WHERE edit_id = $1')
-      db.exec_prepared('delete', [edit_id])
+    def load_memo(db, id)
+      db.exec_prepared('load', [id])
     end
 
-    def updated_memo(title, article, edit_id)
-      db = PG.connect(dbname: 'postgres')
-      db.prepare('update', 'UPDATE Memos SET title = $1, article = $2 WHERE edit_id = $3')
-      db.exec_prepared('update', [title, article, edit_id])
+    def add_memo(db, title, article)
+      db.exec_prepared('add', [title, article])
+    end
+
+    def delete_memo(db, id)
+      db.exec_prepared('delete', [id])
+    end
+
+    def updated_memo(db, title, article, id)
+      db.exec_prepared('update', [title, article, id])
     end
   end
 end
 
-Memo.create_db
+db = PG.connect(dbname: 'postgres')
+Memo.create_db(db)
+Memo.prepared_statesments(db)
 
 get '/' do
-  Memo.create_db
-  @memos = Memo.load_memo.sort_by { |memo| memo['id'] }
+  @memos = Memo.load_all_memos(db).sort_by { |memo| memo['id'] }
   erb :home
 end
 
 post '/' do
-  edit_id = SecureRandom.uuid
-  Memo.add_memo(params[:title], params[:article], edit_id)
+  Memo.add_memo(db, params[:title], params[:article])
   redirect to('/')
 end
 
@@ -61,28 +58,22 @@ get '/new' do
 end
 
 get '/memos/:id' do
-  memo = Memo.load_memo(params[:id])
-  @edit_id = params[:id]
-  @title = memo[0]['title']
-  @article = memo[0]['article']
+  @memo = Memo.load_memo(db, params[:id])
   erb :detail
 end
 
 delete '/memos/:id' do
-  Memo.delete_memo(params[:id])
+  Memo.delete_memo(db, params[:id])
   redirect to('/')
 end
 
 get '/memos/:id/edit' do
-  memo = Memo.load_memo(params[:id])
-  @edit_id = params[:id]
-  @title = memo[0]['title']
-  @article = memo[0]['article']
+  @memo = Memo.load_memo(db, params[:id])
   erb :edit
 end
 
 patch '/memos/:id' do
-  Memo.updated_memo(params[:title], params[:article], params[:id])
+  Memo.updated_memo(db, params[:title], params[:article], params[:id])
   redirect to('/')
 end
 
